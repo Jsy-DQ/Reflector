31af6572c0cecae5d2c82b36822c52cd6373e33e
Turkey J <708879773@qq.com>
2017-08-29 09:33:53 +0800

    Update SourceCostForClassMethodUtil.java

--- SourceCostForClassMethodUtil.java
+++ SourceCostForClassMethodUtil.java
@@ -19,9 +19,9 @@ import util.exception.runtime.IllegalFirstParamException;
  */
 public class SourceCostForClassMethodUtil {
 	private static Runtime r = Runtime.getRuntime();
-
+	//example 
 	public static void main(String[] args) {
-		//example 一个脏婆
+		
 		long temp=System.currentTimeMillis();
 		Object[][][] paramsAndClasses=getParamsAndClasses(new Object[]{1,"321",123l,3213f});
 		Object[][] paramsObj=paramsAndClasses[0];
@@ -38,7 +38,7 @@ public class SourceCostForClassMethodUtil {
 					"the type of firstParam must be int.class to differentiate constructorParams and methodParams");
 		if ((int) obj[0] > obj.length - 1 || (int) obj[0] < 0)
 			throw new FirstIntegerIndexOutOfBoundsException("IndexOutOfRange");
-		// 构造方法变量还是方法变量|参数还是参数类型 0参数|第几个
+		// 构造方法参数还是方法参数|参数还是参数类型 |第几个
 		Object[][][] paramsAndClasses = new Object[2][][];
 		paramsAndClasses[0]=new Object[2][];
 		//定义第二维的类型，强转准备
@@ -72,12 +72,16 @@ public class SourceCostForClassMethodUtil {
 	}
 	
 	public Object doDeclearedReflect(Class<?> targetClass, String methodName, Object... params) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, InstantiationException{
+		if(targetClass==null||methodName==null)
+			throw new NullPointerException();
 		Object[][][] paramsAndClasses=getParamsAndClasses(params);
 		Object[][] paramsObj=paramsAndClasses[0];
 		Class<?>[][] paramsClass=(Class[][]) paramsAndClasses[1];
 		return targetClass.getDeclaredMethod(methodName, paramsClass[1]).invoke(targetClass.getConstructor(paramsClass[0]).newInstance(paramsObj[0]), paramsObj[1]);
 	}
 	public Object doDeclearedReflect(Object targetObj, String methodName, Object... params) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, InstantiationException{
+		if(targetObj==null||methodName==null)
+			throw new NullPointerException();
 		Class<?> targetClass=targetObj.getClass();
 		Object[][][] paramsAndClasses=getParamsAndClasses(params);
 		Object[][] paramsObj=paramsAndClasses[0];
